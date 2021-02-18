f7462b8793abe3cf00e828270fe199a2d4750ccc
韩晓峰 <xiaofeng.han@inrevo.io>
2021-02-18 09:20:41 +0800

    1

--- src/main/java/com/example/soap/demo1/factorybean/DemoJaxWsServerFactoryBean.java
+++ src/main/java/com/example/soap/demo1/factorybean/DemoJaxWsServerFactoryBean.java
@@ -1,9 +1,7 @@
 package com.example.soap.demo1.factorybean;
 
 import com.example.soap.demo1.invoker.DemoJAXWSMethodInvoker;
-import org.apache.cxf.jaxws.JAXWSMethodInvoker;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
-import org.apache.cxf.jaxws.support.JaxWsServiceFactoryBean;
 import org.apache.cxf.service.invoker.Invoker;
 import org.apache.cxf.service.invoker.SingletonFactory;
 
